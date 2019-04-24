53ee9e5469feb32071fd356ad921fe449b9d9d04
Nazim Hickmet <toto@mail.com>
2019-04-24 13:18:27 +0200

    Affichage de la liste des tâches sur la page d'accueil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public taskList = [];
+
+  constructor(private httpClient:HttpClient){
+
+  }
+
+  ionViewDidEnter(){
+    this.httpClient.get("http://localhost:3000/task/")
+    .subscribe(
+      (data:any)=> {
+        this.taskList = data;
+        console.log(data);
+      },
+      err => console.log(err) 
+    );
+  }
 
 }
