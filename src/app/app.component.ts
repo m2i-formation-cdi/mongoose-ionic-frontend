6469b9dba6747ac96b0cf4e02a60d02179c7fd02
Nazim Hickmet <toto@mail.com>
2019-04-24 09:40:06 +0200

    Création du formulaire todoForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Nouvelle tâche',
+      url: '/todo-form',
+      icon: 'checkbox'
     }
   ];
 
