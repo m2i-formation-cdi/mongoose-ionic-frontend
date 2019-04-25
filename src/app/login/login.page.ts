b40141c8f8d3fe8a1c9fceccff80f01c99fba145
Nazim Hickmet <toto@mail.com>
2019-04-25 16:52:07 +0200

    login des utilsateurs

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public loginInfo = {
+    login: '',
+    password: ''
+  };
+
+  constructor(
+    private httpClient:HttpClient,
+    private userService: UserService,
+    private router: Router,
+    private toastCtrl: ToastController) 
+    { }
 
   ngOnInit() {
+  }
+
+  validateLogin(){
+    this.httpClient.post('http://localhost:3000/login', this.loginInfo)
+    .subscribe(
+      (response:any)=>{
+        if(response.found){
+          this.userService.setUser(response.data);
+          this.router.navigateByUrl('/home');
+        } else {
+          let myToast = this.toastCtrl.create(
+            {
+              message: "Infos d'authentification foireuses",
+              duration: 3000
+            }
+          );
+
+          myToast.then((toast)=> toast.present());
+        }
+      },
+      (err) => {console.log(err)}
+    );
   }
 
 }
